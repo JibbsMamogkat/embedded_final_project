bc25811426190f85fa1c627d67dbca5bc4786566
Jameel <sbijjj1@gmail.com>
2026-07-01 18:33:25 +0800

    added repomix to gitignore and fixed naming parameter in roi cropper header

--- main/roi_cropper.h
+++ main/roi_cropper.h
@@ -7,6 +7,6 @@
 void extract_roi_patch(const uint8_t* full_frame, 
                        int frame_width, 
                        int x, int y, int width, int height, 
-                       uint8_t* output_patch_64x64);
+                       uint8_t* output_patch_64x64_rgb);
 
 #endif // ROI_CROPPER_H
